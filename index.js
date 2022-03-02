349c3a67fa8b431cc58f88967bb19d50cc878fa9
Yobany Caltempa <gioclt26@gmail.com>
2022-03-02 17:21:57 -0500

    commit tree 64062b8d385901516a6fc876bbc954fb385537db

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockStart){
+    if (blockStart > 42)
+        return blockStart - 42
+    else (blockStart < 42)
+        return 42 - blockStart
+}
+
+function distanceFromHqInFeet(feetStart){
+    if (feetStart > 42)
+        return (feetStart - 42)*264
+    else (feetStart < 42)
+        return (42 - feetStart)*264
+}
+
+function distanceTravelledInFeet(feetTravel, distanceTravel){
+    if (feetTravel > distanceTravel)
+        return (feetTravel - distanceTravel)*264
+    else (feetTravel < distanceTravel)
+        return (distanceTravel - feetTravel)*264
+}
+
+function calculatesFarePrice (a, b){
+    if  (a < b ) {
+        if (((b - a) * 264) < 400){
+        return 0
+        } else if (((b-a) * 264) < 2000 ){
+        return (((b-a) * 264 - 400) * 0.02)
+        }else if (((a-b)* 264)< 2500) {
+        return 25
+        }else if (((a-b)* 264) >= 2500){
+        return "cannot travel that far"
+        } 
+     }  else if (a >b ){
+         if (((a - b) * 264) < 400){
+         return 0
+        } else if (((a-b) * 264) < 2000 ){
+        return (((a-b) * 264 - 400) * 0.02)
+         } else if (((a-b)* 264) < 2500) {
+         return 25
+        } else if (((a-b)* 264) >= 2500){
+         return 'cannot travel that far'
+        }
+     }
+}
